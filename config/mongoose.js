c0d3236184dac3044460a625b93c3d5226026940
pratik <pratikpriyadarshi1998@gmail.com>
2024-03-20 14:29:34 +0530

    changes in config

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://0.0.0.0:27017/issuetrackerDb');
+mongoose.connect('mongodb://%2Ftmp%2Fmongodb-27017.sock/issuetrackerDb');
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "error in db"));
 db.once("open", function () {
